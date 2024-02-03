441a8734d8004bcf09bed003137b1052ed4ea92a
Jackson Carter <jackson.carter@students.fhu.edu>
2024-02-03 14:11:10 -0600

    fixing fork?
    making sure it's working

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def check_character(word, index):
     else:
         return "unknown"
           
-
+#hopefully fork is working
 if __name__ == '__main__': 
     print(check_character('happy birthday', 2))
     print(check_character('happy birthday', 5))
